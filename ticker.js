e13d8649224a3f10fd3de7e27b1e2c3a1019262b
hmgomez <harvey@1and0.co.uk>
2017-01-22 17:23:37 +0000

    Cleaned up code

--- ticker.js
+++ ticker.js
@@ -3,19 +3,10 @@ const request = require('request');
 const WebSocket = require('ws');
 
 // Global vars
-const webSocket = new WebSocket('wss://api.bitfinex.com/ws/2');
-//const ob = {
-//    event: 'subscribe',
-//    channel: 'book',
-//    symbol: 'tBTCUSD',
-//    prec: 'P2'
-//};
-
+const wsUrl = 'wss://api.bitfinex.com/ws/2';
+const restUrl = 'https://api.bitfinex.com/v2/book/tBTCUSD/P2?len=100';
+const percentage = 5;
 var ticker = { };
-var targetUrl = "https://api.bitfinex.com/v2/book/tBTCUSD/P2?len=100";
-var percentage = 5;
-
-// Payloads
 var payloadTicker = JSON.stringify({
     event: 'subscribe',
     channel: 'ticker',
@@ -32,25 +23,27 @@ function convertCurrency(price, sumOfOrders) {
 }
 
 // WebSocket Work - Subscribe to the ticker channel for price updates.
+var webSocket = new WebSocket(wsUrl);
+
 webSocket.on('open', function() {
     webSocket.send(payloadTicker);
 });
 
-webSocket.on('message', function(msg) {
-    var response = JSON.parse(msg);
+webSocket.on('message', function(data) {
+    var response = JSON.parse(data);
 
     if (response.event) {
         console.log(response);
     }
-    else if (response[1] === "hb") {
-        console.log("polling server...");
+    else if (response[1] === 'hb') {
+        console.log('polling server...');
     }
     else {
         ticker.bid = response[1][0];
         ticker.ask =response[1][2];
         ticker.price = response[1][6];
-        ticker.down5 = ticker.price - calculatePercent(ticker.price, percentage);
-        ticker.up5 = parseInt(ticker.price) + parseInt(calculatePercent(ticker.price, percentage));
+        ticker.lowerPercentage = ticker.price - calculatePercent(ticker.price, percentage);
+        ticker.upperPercentage = parseInt(ticker.price) + parseInt(calculatePercent(ticker.price, percentage));
 
         console.log("Price: $" + ticker.price);
         sumOrders();
@@ -59,28 +52,27 @@ webSocket.on('message', function(msg) {
 
 // REST Work - Poll the api for order book data.
 function sumOrders() {
-    request(targetUrl, function(error, response, body) {
+    request(restUrl, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             var orders = JSON.parse(body);
             var bidSum = 0;
             var askSum = 0;
 
-            for (var i = 0; i < orders.length; i++) {
-                if(orders[i][0] < ticker.price && orders[i][0] > ticker.down5) {
-                    bidSum += parseInt(orders[i][2], 10);
+            orders.forEach(function(order) {
+                if (order[0] < ticker.price && order[0] > ticker.lowerPercentage) {
+                    bidSum += parseInt(order[2]);
+                } else if (order[0] > ticker.price && order[0] < ticker.upperPercentage) {
+                    askSum -= parseInt(order[2]);
                 }
-                else if (orders[i][0] > ticker.price && orders[i][0] < ticker.up5) {
-                    askSum -= parseInt(orders[i][2], 10);
-                }
-            }
-
-            console.log("Bids 5%: " + bidSum + " | " + "Asks 5%: " + askSum);
+            });
 
             var bidDollarValue = convertCurrency(ticker.price, bidSum);
             var askDollarValue = convertCurrency(ticker.price, askSum);
+
+            console.log("Bids 5%: " + bidSum + " | " + "Asks 5%: " + askSum);
             console.log('Bids: $' + bidDollarValue.toLocaleString());
             console.log('Asks: $' + askDollarValue.toLocaleString());
-            console.log("---------------------");
+            console.log("---------------------"); // divider for more readable output
         }
     });
 }
